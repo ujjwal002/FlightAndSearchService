0816f1144796cc4e5962ce83d34ac129bb125bc6
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-03-20 12:44:20 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,7 +6,8 @@ class CityRepository {
       const city = await City.create({ name });
       return city;
     } catch (error) {
-      throw error;
+      console.log("something went wrong in the repository layer");
+      throw { error };
     }
   }
 
@@ -18,7 +19,28 @@ class CityRepository {
         },
       });
     } catch (error) {
-      throw error;
+      console.log("something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      console.log("something went wrong in repository level");
+      throw { error };
+    }
+  }
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+    } catch (error) {
+      console.log("someting went wrong in repository level");
+      throw { error };
     }
   }
 }
